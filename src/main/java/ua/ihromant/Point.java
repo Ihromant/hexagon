d499e4f0ed24e7f66824f422c9ce2d4746f19a4c
Ihromant <vespertilion@gmail.com>
2021-01-12 20:49:41 +0200

    Simple project to show performance bottleneck

---
+++ src/main/java/ua/ihromant/Point.java
@@ -0,0 +1,20 @@
+package ua.ihromant;
+
+public class Point {
+    private final double x;
+    private final double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point add(Point other) {
+        return new Point(this.x + other.x, this.y + other.y);
+    }
+
+    @Override
+    public String toString() {
+        return x + "," + y;
+    }
+}
