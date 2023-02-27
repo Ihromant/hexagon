86b510d1a1d0b5e1c597fc59c3f7bd1e4d8e7d2c
ihromant <vespertilion@gmail.com>
2023-02-27 14:07:31 +0200

    Strengthened blacklist

--- src/main/java/ua/ihromant/IO.java
+++ src/main/java/ua/ihromant/IO.java
@@ -52,9 +52,6 @@ public final class IO {
         if (cls.isInterface()) {
             return true;
         }
-        if (cls.isArray()) {
-            return blackList(cls.componentType());
-        }
         return !cls.assignableTo(IsSerializable.class);
     }
 
