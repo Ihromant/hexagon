00c1c48adbab9cd065ec65ea955fffffda94febf
ihromant <vespertilion@gmail.com>
2023-02-27 11:06:08 +0200

    Got rid of util

--- src/main/java/ua/ihromant/deserializers/Deserializer.java
+++ src/main/java/ua/ihromant/deserializers/Deserializer.java
@@ -6,13 +6,13 @@ import org.teavm.jso.core.JSBoolean;
 import org.teavm.jso.core.JSNumber;
 import org.teavm.jso.core.JSObjects;
 import org.teavm.jso.core.JSString;
-import ua.ihromant.serializers.Util;
 
 public interface Deserializer {
     Deserializer BOOLEAN = jso -> jso.<JSBoolean>cast().booleanValue();
     Deserializer INT = jso -> jso.<JSNumber>cast().intValue();
     Deserializer DOUBLE = jso -> jso.<JSNumber>cast().doubleValue();
     Deserializer STRING = jso -> jso.<JSString>cast().stringValue();
+
     Deserializer BOOLEAN_ARRAY = jso -> {
         JSArray<JSBoolean> boolArray = jso.cast();
         int length = boolArray.getLength();
@@ -44,7 +44,7 @@ public interface Deserializer {
     };
 
     static Deserializer nullable(Deserializer base) {
-        return jso -> JSObjects.isUndefined(jso) || Util.isNull(jso) ? null : base.read(jso);
+        return jso -> jso == null || JSObjects.isUndefined(jso) ? null : base.read(jso);
     }
 
     Object read(JSObject jso);
