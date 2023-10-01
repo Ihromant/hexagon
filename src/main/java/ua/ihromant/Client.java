73a49313c16ea43fe3b0bd8d2ae3c9d8c32d6f5b
ihromant <vespertilion@gmail.com>
2023-10-01 19:44:37 +0300

    Other benchmark

--- src/main/java/ua/ihromant/Client.java
+++ src/main/java/ua/ihromant/Client.java
@@ -1,52 +1,59 @@
 package ua.ihromant;
 
-import org.teavm.jso.dom.html.HTMLDocument;
-import org.teavm.jso.dom.html.HTMLElement;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.IntSupplier;
 
 public class Client {
-    private static final int HEXAGON_RADIUS = 25;
-    private static final int X_TOP = 100;
-    private static final int Y_TOP = 100;
     public static void main(String[] args) {
-        HTMLDocument document = HTMLDocument.current();
-        HTMLElement svg = HTMLDocument.current().createElementNS("http://www.w3.org/2000/svg", "svg").cast();
-        svg.setAttribute("width", "800");
-        svg.setAttribute("height", "600");
-        for (int i = 0; i < Field.WIDTH; i++) {
-            for (int j = 0; j < Field.HEIGHT; j++) {
-                Field field = new Field(i, j);
-                HTMLElement poly = computePolygon(field, computeCenter(field));
-                svg.appendChild(poly);
-            }
+        int[] integers = new int[1_000_000];
+        int sink = 0;
+        fill(integers, ThreadLocalRandom.current()::nextInt);
+        for (int i : integers) {
+            sink += Integer.toString(i).length();
         }
-        document.getBody().appendChild(svg);
-    }
-
-    private static HTMLElement computePolygon(Field field, Point center) {
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < 5; i++) {
-            builder.append(center.add(shift(i)).toString());
-            builder.append(' ');
+        System.out.println("Warmup: " + sink);
+        fill(integers, ThreadLocalRandom.current()::nextInt);
+        long time = System.currentTimeMillis();
+        for (int i : integers) {
+            sink += Integer.toString(i).length();
         }
-        builder.append(center.add(shift(5)).toString());
-        HTMLElement poly = HTMLDocument.current().createElementNS("http://www.w3.org/2000/svg", "polygon").cast();
-        poly.setId(field.getX() + "_" + field.getY());
-        poly.setAttribute("points", builder.toString());
-        poly.setAttribute("stroke", "limegreen");
-        poly.setAttribute("fill", "none");
-        return poly;
+        System.out.println("Mixed integers: " + (System.currentTimeMillis() - time) + ", sink: " + sink);
+        fill(integers, () -> ThreadLocalRandom.current().nextInt(-99, 100));
+        time = System.currentTimeMillis();
+        for (int i : integers) {
+            sink += Integer.toString(i).length();
+        }
+        System.out.println("Small integers: " + (System.currentTimeMillis() - time) + ", sink: " + sink);
+        fill(integers, () -> ThreadLocalRandom.current().nextBoolean()
+                ? ThreadLocalRandom.current().nextInt(Integer.MIN_VALUE, -100_000_000)
+                : ThreadLocalRandom.current().nextInt(100_000_000, Integer.MAX_VALUE));
+        time = System.currentTimeMillis();
+        for (int i : integers) {
+            sink += Integer.toString(i).length();
+        }
+        System.out.println("Large integers: " + (System.currentTimeMillis() - time) + ", sink: " + sink);
+        fill(integers, ThreadLocalRandom.current()::nextInt);
+        time = System.currentTimeMillis();
+        for (int i = 0; i < integers.length; i++) {
+            int val = integers[i];
+            sink += Integer.toString(val, i % 30 + 2).length();
+        }
+        System.out.println("Integers with radix: " + (System.currentTimeMillis() - time) + ", sink: " + sink);
     }
 
-    private static Point shift(int number) {
-        double ang = Math.PI / 2 - number * Math.PI / 3;
-        return new Point(HEXAGON_RADIUS * Math.cos(ang), HEXAGON_RADIUS * Math.sin(ang));
+    private static void fill(int[] values, IntSupplier generator) {
+        for (int i = 0; i < values.length; i++) {
+            values[i] = generator.getAsInt();
+        }
     }
 
-    public static Point computeCenter(Field f) {
-        int xSub = 2 * f.getX() - f.getY() % 2;
-        int ySub = 3 * f.getY();
-        double xSwap = Math.sin(Math.PI / 3) * HEXAGON_RADIUS * xSub;
-        double ySwap = Math.cos(Math.PI / 3) * HEXAGON_RADIUS * ySub;
-        return new Point(X_TOP + xSwap, Y_TOP + ySwap);
+    private static String generateTag() {
+        return ThreadLocalRandom.current().nextInt(2) == 0 ? "div" : "span";
     }
+
+    private static Point generatePoint() {
+        return new Point(ThreadLocalRandom.current().nextInt(0, 800), ThreadLocalRandom.current().nextInt(0, 600));
+    }
+
+    private record Point(int x, int y) {}
 }
